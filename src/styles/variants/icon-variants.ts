b233cec856b70d6dc963e137063cb5feffe9d612
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2025-11-26 17:17:52 -0300

    Reduce icon variant size

--- src/styles/variants/icon-variants.ts
+++ src/styles/variants/icon-variants.ts
@@ -3,7 +3,7 @@ import { cva } from 'class-variance-authority';
 export const IconVariants = cva('', {
    variants: {
       size: {
-         md: 'size-8',
+         md: 'size-5',
       },
    },
    defaultVariants: {
