c84164a0b568e3fa8c13b90efe5aa830b92e05c5
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2025-11-28 20:27:42 -0300

    Adds primary variant

--- src/styles/variants/icon-variants.ts
+++ src/styles/variants/icon-variants.ts
@@ -2,11 +2,15 @@ import { cva } from 'class-variance-authority';
 
 export const IconVariants = cva('', {
    variants: {
+      variant: {
+         primary: 'fill-yellow',
+      },
       size: {
          md: 'size-5',
       },
    },
    defaultVariants: {
+      variant: 'primary',
       size: 'md',
    },
 });
