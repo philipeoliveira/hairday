947a558f616f4f48d93578109847a626fc6ffbbb
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2025-12-03 17:42:33 -0300

    Refactor the code to include the Logo, Sidebar, and Schedule components

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,14 @@
-import { Text } from './components/Text';
-import { Button } from './components/Button';
-import { AppointmentCard } from './core-components/AppointmentCard';
-import { InputText } from './components/InputText';
-import UserSquare from './assets/icons/userSquare.svg?react';
+import Logo from '../src/core-components/Logo';
+import { Schedule } from './core-components/Schedule';
+import { Sidebar } from './core-components/Sidebar';
 
 function App() {
-   const morningAppointment = '09:00 Fulano Silva';
-   const afternoonAppointment = '14:00 Beltrano';
-   const nightAppointment = '';
-
    return (
-      <div className='space-y-4'>
-         <Text as='h1' variant='body-title-lg' className='text-gray-100'>
-            Agende um atendimento
-         </Text>
-         <Text variant='body-text-sm' className='text-gray-300'>
-            Selecione data, horário e informe o nome do cliente para criar o agendamento
-         </Text>
-
-         <Button>Agendar</Button>
-
-         <Text as='h2' variant='body-title-lg' className='text-gray-100'>
-            Sua agenda
-         </Text>
-         <Text variant='body-text-sm' className='text-gray-300'>
-            Consulte os seus cortes de cabelo agendados por dia
-         </Text>
-
-         <InputText icon={UserSquare} />
-
-         <div className='space-y-3'>
-            <AppointmentCard period='morning'>{morningAppointment}</AppointmentCard>
-            <AppointmentCard period='afternoon'>{afternoonAppointment}</AppointmentCard>
-            <AppointmentCard period='night'>{nightAppointment}</AppointmentCard>
-         </div>
-      </div>
+      <main className='relative p-3 flex gap-3 flex-col md:flex-row max-w-360 mx-auto'>
+         <Logo />
+         <Sidebar />
+         <Schedule />
+      </main>
    );
 }
 
