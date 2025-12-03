9b53481c5f852728f529ee401c309b5d29825833
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2025-12-03 17:38:49 -0300

    Add props to the component

--- src/components/InputText.tsx
+++ src/components/InputText.tsx
@@ -1,16 +1,21 @@
 import { Icon } from './Icon';
 import { inputWrapperVariants, inputVariants } from '../styles/variants/input-variants';
 
-interface InputText {
+interface InputTextProps extends React.ComponentProps<'input'> {
    icon?: React.ComponentProps<typeof Icon>['svg'];
    className?: string;
 }
 
-export function InputText({ icon, className }: InputText) {
+export function InputText({ icon, className, ...props }: InputTextProps) {
    return (
       <div className={inputWrapperVariants({ className })}>
          {icon && <Icon svg={icon} />}
-         <input type='text' className={inputVariants()} placeholder='Nome do cliente' />
+         <input
+            type='text'
+            className={inputVariants()}
+            placeholder='Nome do cliente'
+            {...props}
+         />
       </div>
    );
 }
