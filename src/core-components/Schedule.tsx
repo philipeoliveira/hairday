1669d912e2851121dd43d3d5d6c96d423b0bfc49
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2025-12-04 17:59:11 -0300

    List of appointment contents

--- src/core-components/Schedule.tsx
+++ src/core-components/Schedule.tsx
@@ -1,10 +1,20 @@
 import { Text } from '../components/Text';
 import { AppointmentCard } from '../core-components/AppointmentCard';
+import { useAppointment } from '../hooks/useAppointment';
+import { times } from '../constants/times';
+import { AppointmentCardItem } from './AppointmentCardItem';
 
 export function Schedule() {
-   const morningAppointment = '09:00 Fulano Silva';
-   const afternoonAppointment = '14:00 Beltrano';
-   const nightAppointment = '';
+   const { appointments } = useAppointment();
+   const morningAppointments = appointments.filter((appointment) =>
+      (times.morning as readonly string[]).includes(appointment.time)
+   );
+   const afternoonAppointments = appointments.filter((appointment) =>
+      (times.afternoon as readonly string[]).includes(appointment.time)
+   );
+   const nightAppointments = appointments.filter((appointment) =>
+      (times.night as readonly string[]).includes(appointment.time)
+   );
 
    return (
       <div className='w-full py-20'>
@@ -20,11 +30,56 @@ export function Schedule() {
                </div>
             </div>
             <div className='space-y-3'>
-               <AppointmentCard period='morning'>{morningAppointment}</AppointmentCard>
-               <AppointmentCard period='afternoon'>
-                  {afternoonAppointment}
+               <AppointmentCard period='morning'>
+                  {morningAppointments.length > 0 ? (
+                     morningAppointments.map((morningAppointment) => (
+                        <AppointmentCardItem
+                           key={morningAppointment.id}
+                           id={morningAppointment.id}
+                           client={morningAppointment.client}
+                           time={morningAppointment.time}
+                        />
+                     ))
+                  ) : (
+                     <Text variant='body-text-sm' className='text-gray-300'>
+                        Nenhum agendamento para o período da manhã
+                     </Text>
+                  )}
                </AppointmentCard>
-               <AppointmentCard period='night'>{nightAppointment}</AppointmentCard>
+
+               <AppointmentCard period='afternoon'>
+                  {afternoonAppointments.length > 0 ? (
+                     afternoonAppointments.map((afternoonAppointment) => (
+                        <AppointmentCardItem
+                           key={afternoonAppointment.id}
+                           id={afternoonAppointment.id}
+                           client={afternoonAppointment.client}
+                           time={afternoonAppointment.time}
+                        />
+                     ))
+                  ) : (
+                     <Text variant='body-text-sm' className='text-gray-300'>
+                        Nenhum agendamento para o período da tarde
+                     </Text>
+                  )}
+               </AppointmentCard>
+
+               <AppointmentCard period='night'>
+                  {nightAppointments.length > 0 ? (
+                     nightAppointments.map((nightAppointment) => (
+                        <AppointmentCardItem
+                           key={nightAppointment.id}
+                           id={nightAppointment.id}
+                           client={nightAppointment.client}
+                           time={nightAppointment.time}
+                        />
+                     ))
+                  ) : (
+                     <Text variant='body-text-sm' className='text-gray-300'>
+                        Nenhum agendamento para o período da noite
+                     </Text>
+                  )}
+               </AppointmentCard>
             </div>
          </div>
       </div>
